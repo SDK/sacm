967d5443989d2eec55379263c0705ffa87097423
Sebastian Gonzalez <sagonzal@alma.cl>
2014-07-10 22:14:13 -0400

    first try to get info from xml

--- checker.py
+++ checker.py
@@ -1,5 +1,12 @@
 __author__ = 'sdk'
 from sacm import *
+from xml.dom import minidom
 
 asdmXML = open('uid___A002_X837c61_Xffd/ASDM.xml', 'r')
+
+asdm = minidom.parse(asdmXML)
+rows = asdm.getElementsByTagName('Table')
+for i in rows:
+    print i
+
 
