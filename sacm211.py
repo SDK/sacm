fixed bug where the coordinates were badly calculated , correct formula provided by Sawada-san

sacm211.py
@@ -12,7 +12,8 @@ import sacm.geo_helper as gh
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 import dateutil.parser as prs
-
+import math
+import pylab as pl
 pd.options.display.width = 300
 
 J0 = ephem.julian_date(0)
@@ -26,6 +27,19 @@ def azelToRaDec(az=None, el=None,lat=None,lon=None,alt=None, ut=None):
     observer.date = ut - J0
     return observer.radec_of(az, el)
 
+def measure(lat1, lon1, lat2, lon2):
+    R = 6378.137 # Radius of earth in KM
+    dLat = (lat2 - lat1) * np.pi / 180.
+    dLon = (lon2 - lon1) * np.pi / 180.
+    a = math.sin(dLat/2.) * math.sin(dLat/2.) + math.cos(lat1 * np.pi / 180.) * math.cos(lat2 * np.pi / 180.) * math.sin(dLon/2.) * math.sin(dLon/2.)
+    c = 2. * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = R * c
+    return d * 1000. # meters
+
+def rot(Pl, rLong, rLat):
+    return [pl.cos(rLong)*pl.cos(rLat) * Pl[0] - pl.sin(rLong) * Pl[1] - pl.cos(rLong)*pl.sin(rLat) * Pl[2],
+           pl.sin(rLong)*pl.cos(rLat) * Pl[0] + pl.cos(rLong) * Pl[1] - pl.sin(rLong)*pl.sin(rLat) * Pl[2],
+           pl.sin(rLat) * Pl[0] + pl.cos(rLat) * Pl[2]]
 
 parser = ASDMParseOptions()
 parser.asALMA()
@@ -60,6 +74,8 @@ for idx,row in enumerate(rows):
     pointingList.append((idx, str(row.antennaId()), float(row.numSample()), float(row.numTerm()),str(row.timeInterval()), str(row.timeOrigin())))
 
 pointingAll = pd.DataFrame(pointingList,columns = ['rowNum','antennaId','samples','iter','duration','origin'])
+
+#TODO: Fix to match any antenna
 pointing = pointingAll[pointingAll['antennaId'] == 'Antenna_1']
 
 #do some transformations for matching the data
@@ -85,7 +101,6 @@ geo['pos'] = geo.apply(lambda x: arrayParser(x['position'],1) , axis = 1 )
 geo['lat'], geo['lon'], geo['alt'] = zip(*geo.apply(lambda x: gh.turn_xyz_into_llh(float(x.pos[0]),float(x.pos[1]),float(x.pos[2]), 'wgs84'),axis=1))
 field['ra'],field['dec'] = zip(*field.apply(lambda x: arrayParser(x['referenceDir'],2)[0], axis = 1))
 
-#TODO: Fix to match any antenna
 
 correctedList = list()
 correctedList.append((ra,dec,0))
@@ -116,7 +131,16 @@ partId =  target[target['ObsParameter'] == science.entityPartId.values[0]].Field
 
 predicted = sboffset[sboffset['partId'] == partId][['latitude','longitude']]
 longitude, lat = sbfield[sbfield['entityPartId'] == partId][['longitude','latitude']].values[0]
-predicted[['latitude','longitude']] = predicted[['latitude','longitude']].astype(float)
+predicted[['raoff','decoff']] = predicted[['longitude','latitude']].astype(float)
+RA0 = float(longitude)*pl.pi/180.
+Dec0 = float(lat)*pl.pi/180.
+
+predicted['dRA'] = predicted.apply(lambda x: pl.radians(x['raoff']/3600.), axis = 1)
+predicted['dDec'] = predicted.apply(lambda x: pl.radians(x['decoff']/3600.), axis = 1)
+predicted['Pl'] = predicted.apply(lambda x: list((pl.cos(x['dRA'])*pl.cos(x['dDec']), pl.sin(x['dRA'])*pl.cos(x['dDec']), pl.sin(x['dDec']))) , axis = 1)
+predicted['Ps'] = predicted.apply(lambda x: rot(x['Pl'],RA0,Dec0), axis = 1)
+predicted['otcoor'] = predicted.apply(lambda x: list((pl.arctan2(x['Ps'][1], x['Ps'][0]) % (2.*pl.pi), pl.arcsin(x['Ps'][2]))), axis =1)
+
 predictedList = list()
 predictedList.append((float(longitude)*np.pi/180.,float(lat)*np.pi/180. ))
 