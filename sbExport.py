07b947673ebc1c25cd035f848448486d74e4fc9b
Sebastian Gonzalez <seba.cl@gmail.com>
2016-03-09 10:33:39 -0300

    added SBexporter of Metadata

--- sbExport.py
+++ sbExport.py
@@ -16,14 +16,15 @@ sbUID = sys.argv[1]
 if 'uid___' in sbUID:
     sbUID = sbUID.replace('___','://').replace('_','_')
 
-sql = '''select se_eb_uid from ALMA.SHIFTLOG_ENTRIES where SE_SB_ID = "%s" and SE_QA0FLAG = "Pass"'''%sbUID
+sql = """select se_eb_uid from ALMA.SHIFTLOG_ENTRIES where SE_SB_ID = '%s' and SE_QA0FLAG = 'Pass'"""%sbUID
+#print sql
 
 rows = cursor.execute(sql)
 sbUID_norma = sbUID.replace('://','___').replace('/','_')
 
 if rows > 0:
-    os.mkdir(sbUID_norma)
-    os.chdir(sbUID_norma)
+    os.mkdir('SB_'+sbUID_norma)
+    os.chdir('SB_'+sbUID_norma)
     for i in rows:
         subprocess.Popen(['asdmExport','-m',i[0]])
     os.chdir('../')
